b9e893d6106fa12439d0348984d717b305d224b6
sajal <sajalsbaral@gmail.com>
2023-11-29 13:25:43 -0500

    created a function to create a default grid of 16x16

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 const container = document.getElementById('container');
 
+function multiply(num) {
+    return num * num
+}
+
 function makeDivs(numDivs) {
-    for(let i = 0; i < numDivs; i++) {
+    let gridSize = multiply(numDivs)
+
+    for(let i = 0; i < gridSize; i++) {
         let cells = document.createElement('div');
         container.appendChild(cells)
     }
 }
 
-console.log(makeDivs(16))
+function defaultGrid() {
+    makeDivs(16)
+}
+
+makeDivs(24)
